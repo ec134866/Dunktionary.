15cd67d400443c1e1f7dc11faad42de61fb0b96d
ec134866 <evan@thecarters.com>
2023-08-09 01:20:45 -0600

    possibly

--- dunktionaryPRJ/settings.py
+++ dunktionaryPRJ/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT= os.path.join(BASE_DIR, "staticfiles")
 
 
 # Default primary key field type
