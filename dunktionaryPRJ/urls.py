49629569dc4be26a66a89112ecb5263a40611154
ec134866 <evan@thecarters.com>
2023-08-09 01:49:38 -0600

    test

--- dunktionaryPRJ/urls.py
+++ dunktionaryPRJ/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-admin.autodiscover()
-admin.site.enable_nav_sidebar = False
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dunktionaryApp.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
