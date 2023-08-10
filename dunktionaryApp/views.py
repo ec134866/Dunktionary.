997e42a006e8e66d6d27f91bbabe3eeb067e1d8b
ec134866 <evan@thecarters.com>
2023-08-09 22:41:09 -0600

    combineoldwnew

--- dunktionaryApp/views.py
+++ dunktionaryApp/views.py
@@ -91,15 +91,23 @@ def passPageView(request, pass_altName):
 #     return render(request, "dunktionaryApp/search.html", context)
 
 def searchPageView(request):
-    query = request.GET.get('name', '')
+    try:
+        name = request.GET['name']
 
-    # Perform full-text search on the 'name' field using MySQL's MATCH AGAINST
-    db_dunks = Dunk.objects.filter(
-        Q(name__search=query) | Q(name__icontains=query)
-    )
-    db_passes = Pass.objects.filter(
-        Q(name__search=query) | Q(name__icontains=query)
-    )
+        db_dunks = Dunk.objects.filter(
+            Q(name__search=name) | Q(name__icontains=name)
+        )
+    except:
+        db_dunks = Dunk.objects.all()
+
+    try:
+        name = request.GET['name']
+
+        db_passes = Pass.objects.filter(
+            Q(name__search=name) | Q(name__icontains=name)
+        )
+    except: 
+        db_passes = Pass.objects.all()
 
     context = {
         'dunks': db_dunks,
