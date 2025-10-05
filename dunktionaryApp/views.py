6be5c13e695b2ff6dd12bf3f04a55d446fe00ee0
ec134866 <evan@thecarters.com>
2025-10-05 16:39:05 -0400

    change clamp of people to 30 not 10, and comment for where to adjust min maxes

--- dunktionaryApp/views.py
+++ dunktionaryApp/views.py
@@ -96,7 +96,8 @@ def searchPageView(request):
 
 def trainPageView(request):
 	if request.method == 'POST':
-		num_people = max(1, min(int(request.POST.get('num_people', 1)), 10))
+		# update to min/max also needed in trainmaker.html 
+		num_people = max(1, min(int(request.POST.get('num_people', 1)), 30))
 		level = max(0, min(int(request.POST.get('level', 1)), 9))
 		train = [] 
 		total_score = 0  
